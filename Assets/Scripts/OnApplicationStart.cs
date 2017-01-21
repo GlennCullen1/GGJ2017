5d5780dc6ae278af5bfeac09decd9557a9e2d1ce
unknown <1200939@MC10783.uad.ac.uk>
2017-01-21 01:15:49 +0000

    Added basic explosions and planet movement

---
+++ Assets/Scripts/OnApplicationStart.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnApplicationStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CreateScreenBoundries();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreateScreenBoundries()
+    {
+        float colDepth = 4f;
+        float zPosition = 0f;
+        Vector2 screenSize;
+        Transform topCollider;
+        Transform bottomCollider;
+        Transform leftCollider;
+        Transform rightCollider;
+        Vector3 cameraPos;
+           //Generate our empty objects
+        topCollider = new GameObject().transform;
+        bottomCollider = new GameObject().transform;
+        rightCollider = new GameObject().transform;
+        leftCollider = new GameObject().transform;
+
+        //Name our objects 
+        topCollider.name = "TopCollider";
+        bottomCollider.name = "BottomCollider";
+        rightCollider.name = "RightCollider";
+        leftCollider.name = "LeftCollider";
+
+        //Add the colliders
+        topCollider.gameObject.AddComponent<BoxCollider>();
+        bottomCollider.gameObject.AddComponent<BoxCollider>();
+        rightCollider.gameObject.AddComponent<BoxCollider>();
+        leftCollider.gameObject.AddComponent<BoxCollider>();
+
+        //Make them the child of whatever object this script is on, preferably on the Camera so the objects move with the camera without extra scripting
+        topCollider.parent = transform;
+        bottomCollider.parent = transform;
+        rightCollider.parent = transform;
+        leftCollider.parent = transform;
+
+        
+        //Generate world space point information for position and scale calculations
+        cameraPos = Camera.main.transform.position;
+        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
+
+        //Change our scale and positions to match the edges of the screen...   
+        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
+        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
+        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
+        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
+        topCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
+        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f), zPosition);
+        bottomCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
+        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f), zPosition);
+    }
+}
---
+++ Assets/Scripts/TestExplosionOnClick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestExplosionOnClick : MonoBehaviour {
+
+ public float distance = 20f;
+
+
+void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("BOOM");
+            CauseExplosionAtPoint(CastRayToWorld(Input.mousePosition), 5.0f, 500.0f);
+        }
+    }
+
+
+ Vector3 CastRayToWorld(Vector3 coords)
+     {
+        Ray ray = Camera.main.ScreenPointToRay(coords);
+        Vector3 point = ray.origin + (ray.direction * distance);
+        Debug.Log(point);
+        return point;
+     }
+
+    void CauseExplosionAtPoint(Vector3 point, float explosionRadius, float power)
+    {
+        var planetLayer = 1 << LayerMask.NameToLayer("Planets");
+        Collider[] colliders = Physics.OverlapSphere(point, explosionRadius, planetLayer);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                Debug.Log("Explosion Expected " +rb.gameObject.name);
+                rb.AddExplosionForce(power, point, explosionRadius,0.0F);
+        }
+    }
+}
